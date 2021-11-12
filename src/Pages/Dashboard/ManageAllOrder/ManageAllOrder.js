d5d95558b3ed25d2adcadaf82ee07e44c6c18f25
Hafiz <hasnathafiz229>
2021-11-13 00:11:54 +0600

    Manage All Order Get

---
+++ src/Pages/Dashboard/ManageAllOrder/ManageAllOrder.js
@@ -0,0 +1,65 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+const ManageAllOrder = ({ allOrder, handleDelete }) => {
+  const { _id, name, email, order, price, address, phone, status } = allOrder;
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <Card sx={{ maxWidth: "auto" }}>
+        <CardContent sx={{ height: "350px" }}>
+          <Typography
+            style={{ fontWeight: 500 }}
+            gutterBottom
+            variant="h5"
+            component="div"
+          >
+            {order}
+          </Typography>
+          <br />
+          <Typography variant="body2" color="text.secondary">
+            {name}
+          </Typography>
+          <br />
+          <Typography variant="body2" color="text.secondary">
+            {email}
+          </Typography>
+          <br />
+          <Typography variant="body2" color="text.secondary">
+            ${price}
+          </Typography>
+          <br />
+          <Typography variant="body2" color="text.secondary">
+            {address}
+          </Typography>
+          <br />
+          <Typography variant="body2" color="text.secondary">
+            {phone}
+          </Typography>
+          <br />
+          <Typography variant="h6" color="red">
+            {status}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "orange", marginBottom: "10px" }}
+            size="small"
+            onClick={() => handleDelete(_id)}
+          >
+            Cancel Order
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
+};
+
+export default ManageAllOrder;
--- src/Pages/Dashboard/ManageAllOrders/ManageAllOrders.js
+++ src/Pages/Dashboard/ManageAllOrders/ManageAllOrders.js
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Grid, Typography, Box } from "@mui/material";
+import ManageAllOrder from "../ManageAllOrder/ManageAllOrder";
 
 const ManageAllOrders = () => {
+  const [allOrders, setAllOrders] = useState([]);
+
+  // get all orders
+  useEffect(() => {
+    fetch("http://localhost:5000/orders")
+      .then((res) => res.json())
+      .then((data) => setAllOrders(data));
+  }, []);
+
+  // delete an order
+  const handleDelete = (id) => {
+    // ask for a confirmation before delete an order
+    const sure = window.confirm("Are you sure, you want to continue?");
+    if (sure === true) {
+      const url = `http://localhost:5000/orders/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount) {
+            alert("Deleted Successfully");
+            const remaining = allOrders.filter(
+              (allOrder) => allOrder._id !== id
+            );
+            setAllOrders(remaining);
+          }
+        });
+    }
+  };
   return (
     <div>
-      <h2>Manage All Orders (Admin Only)</h2>
+      <Box sx={{ flexGrow: 1 }}>
+        <Container>
+          <Typography
+            sx={{ fontWeight: 600, m: 2 }}
+            variant="h4"
+            component="div"
+          >
+            Manage All Orders (Admin Only)
+          </Typography>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+            style={{ textAlign: "left" }}
+          >
+            {allOrders.map((allOrder) => (
+              <ManageAllOrder
+                key={allOrder._id}
+                allOrder={allOrder}
+                handleDelete={handleDelete}
+              ></ManageAllOrder>
+            ))}
+          </Grid>
+        </Container>
+      </Box>
     </div>
   );
 };
