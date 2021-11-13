04130cc8b98e0f92c1643c7fcc53b26ee5a4282d
Hafiz <hasnathafiz229>
2021-11-13 10:03:23 +0600

    Admin Reloading fixed

--- src/Pages/Login/AdminRoute/AdminRoute.js
+++ src/Pages/Login/AdminRoute/AdminRoute.js
@@ -4,8 +4,8 @@ import { Redirect, Route } from "react-router";
 import useAuth from "../../../hooks/useAuth";
 
 const AdminRoute = ({ children, ...rest }) => {
-  const { user, admin, isLoading } = useAuth();
-  if (isLoading) {
+  const { user, admin } = useAuth();
+  if (!admin) {
     return <CircularProgress />;
   }
   return (
