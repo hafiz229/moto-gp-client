1e9adea66a364fb346525d023340b28582e7d781
Hafiz <hasnathafiz229>
2022-01-06 15:59:43 +0600

    responsive banner & nav

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,23 +5,67 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Banner.css";
 
-const verticalCenter = {
-  display: "flex",
-  alignItems: "center",
-  height: 400,
-};
-
 const Banner = () => {
   return (
-    <div className="responsive-banner">
-      <Container>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2}>
+    <Container
+      sx={{
+        display: { xs: "block", md: "flex" },
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+      className="responsive-banner"
+    >
+      <Grid item style={{ textAlign: "left" }} xs={12}>
+        <Typography variant="h3" sx={{ fontWeight: 500 }}>
+          We Provide{" "}
+          <Box
+            variant="h3"
+            sx={{
+              color: "orange",
+              display: "inline-block",
+              fontWeight: 500,
+            }}
+          >
+            Premium Bikes
+          </Box>{" "}
+          With High Quality
+        </Typography>
+        <Typography
+          variant="h6"
+          sx={{ my: 3, fontSize: 16, fontWeight: 300, color: "gray" }}
+        >
+          Our company was built by trust and reputation. We know how to make our
+          customers love us by our high quality products with affordable prices.
+          We promise to make you have the interesting experiences.
+        </Typography>
+
+        <NavLink to="/explore" style={{ textDecoration: "none" }}>
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "orange",
+            }}
+          >
+            Explore
+          </Button>
+        </NavLink>
+      </Grid>
+      <Grid item xs={10}>
+        <Box sx={{ marginLeft: "10%", marginRight: "10%" }}>
+          <img
+            src="https://i.ibb.co/x6dmmsv/262-2622065-yellow-bajaj-pulsar-150-rs.png"
+            alt=""
+            style={{ width: "120%" }}
+            // style={{ width: "368px", height: "350px" }}
+          />
+        </Box>
+      </Grid>
+      {/* <Grid container spacing={2}>
             <Grid
               item
               style={{ ...verticalCenter, textAlign: "left" }}
               xs={12}
-              md={7}
+              md={6}
             >
               <Box>
                 <Typography variant="h3" sx={{ fontWeight: 500 }}>
@@ -62,15 +106,13 @@ const Banner = () => {
             </Grid>
             <Grid item xs={12} md={5} sx={{ my: 3 }} className="banner-img">
               <img
-                style={{ verticalCenter, width: "500px" }}
+                style={{ verticalCenter }}
                 src="https://i.ibb.co/x6dmmsv/262-2622065-yellow-bajaj-pulsar-150-rs.png"
                 alt=""
               />
             </Grid>
-          </Grid>
-        </Box>
-      </Container>
-    </div>
+          </Grid> */}
+    </Container>
   );
 };
 
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -1,10 +1,60 @@
 import React from "react";
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  ClickAwayListener,
+  Divider,
+  Grow,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 
 const Navigation = () => {
   const { user, logOut } = useAuth();
+  console.log(user);
+  // react menu component
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
@@ -34,18 +84,84 @@ const Navigation = () => {
               <Button color="inherit">Explore</Button>
             </NavLink>
             {user?.email ? (
-              <Box>
-                <NavLink
-                  style={{ textDecoration: "none", color: "white" }}
-                  to="/dashboard"
-                >
-                  <Button color="inherit">Dashboard</Button>
-                </NavLink>
-                <Button color="inherit">{user.displayName}</Button>
-                <Button onClick={logOut} color="inherit">
-                  Logout
-                </Button>
-              </Box>
+              // <Box>
+              //   <NavLink
+              //     style={{ textDecoration: "none", color: "white" }}
+              //     to="/dashboard"
+              //   >
+              //     <Button color="inherit">Dashboard</Button>
+              //   </NavLink>
+              //   <Button color="inherit">{user.displayName}</Button>
+              //   <Button onClick={logOut} color="inherit">
+              //     Logout
+              //   </Button>
+              // </Box>
+              <Stack direction="row" spacing={2}>
+                <div>
+                  <Button
+                    ref={anchorRef}
+                    id="composition-button"
+                    aria-controls={open ? "composition-menu" : undefined}
+                    aria-expanded={open ? "true" : undefined}
+                    aria-haspopup="true"
+                    onClick={handleToggle}
+                    sx={{ fontWeight: "bolder", color: "white" }}
+                  >
+                    {user.displayName}
+                  </Button>
+                  <Popper
+                    open={open}
+                    anchorEl={anchorRef.current}
+                    role={undefined}
+                    placement="bottom-start"
+                    transition
+                    disablePortal
+                  >
+                    {({ TransitionProps, placement }) => (
+                      <Grow
+                        {...TransitionProps}
+                        style={{
+                          transformOrigin:
+                            placement === "bottom-start"
+                              ? "left top"
+                              : "left bottom",
+                        }}
+                      >
+                        <Paper>
+                          <ClickAwayListener onClickAway={handleClose}>
+                            <MenuList
+                              autoFocusItem={open}
+                              id="composition-menu"
+                              aria-labelledby="composition-button"
+                              onKeyDown={handleListKeyDown}
+                            >
+                              <NavLink
+                                style={{
+                                  textDecoration: "none",
+                                }}
+                                to="/dashboard"
+                              >
+                                <MenuItem
+                                  sx={{ fontWeight: "bolder", color: "black" }}
+                                >
+                                  Dashboard
+                                </MenuItem>
+                              </NavLink>
+                              <Divider />
+                              <MenuItem
+                                onClick={logOut}
+                                sx={{ fontWeight: "bolder" }}
+                              >
+                                Logout
+                              </MenuItem>
+                            </MenuList>
+                          </ClickAwayListener>
+                        </Paper>
+                      </Grow>
+                    )}
+                  </Popper>
+                </div>
+              </Stack>
             ) : (
               <NavLink
                 style={{ textDecoration: "none", color: "white" }}
